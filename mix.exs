cbae56338c84bd872bfce58b7d92402896372181
Taro <taronull@gmail.com>
2024-04-25 11:25:42 +0900

    Support kebab-case in path

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mar.MixProject do
   def project do
     [
       app: :mar,
-      version: "0.2.5",
+      version: "0.2.6",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
